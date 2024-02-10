c55eb11ba301d497bee3a04f709dbf2827b15535
surya-kamal-sholihin <78074770+surya-kamal-sholihin@users.noreply.github.com>
2024-02-10 22:48:18 +0700

    Add files via upload

--- stream.py
+++ stream.py
@@ -1,7 +1,19 @@
 # Import Library
 import pickle
 import streamlit as st
+import mysql.connector
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Connect to mysql server
+mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="mendokusai",
+            database="streamlit",
+        )
+mycursor = mydb.cursor()
+print('Connection Established')
 
 #load save model
 model_NB = pickle.load(open('model_NB.sav', 'rb'))
@@ -12,33 +24,64 @@ tfidf = TfidfVectorizer
 
 loaded_vec = TfidfVectorizer(decode_error='replace', vocabulary = set(pickle.load(open('new_selected_feature_tf-idf.sav', 'rb'))))
 
+# bikin side bar
+
 # Judul halaman
 st.title('klasifikasi Berita Pemilu')
 
+# Bikin menu sidebar
+menu = st.sidebar.selectbox('Pilih Fungsi',('Klasifikasi', 'History'))
+
+if menu == 'Klasifikasi' :
 # Input Berita
-clean_teks = st.text_input('Masukan Teks Berita')
+  teks = st.text_input('Masukan Teks Berita')
 
-fakenews_detection_NB = ''
-fakenews_detection_SVM = ''
+  detect_NB = ''
+  detect_SVM = ''
 
-if st.button('Hasil Deteksi'):
-  #Fungsi Prediksi SVM
-  predict_NB = model_NB.predict(loaded_vec.fit_transform([clean_teks]))
+  if st.button('Hasil Deteksi'):
+    #Fungsi Prediksi SVM
+    predict_NB = model_NB.predict(loaded_vec.fit_transform([teks]))
 
-  if (predict_NB == 0):
-    fakenews_detection_NB = 'Fake News'
+    if (predict_NB == 0):
+      detect_NB = 'Fake News'
 
-  elif (predict_NB == 1):
-    fakenews_detection_NB = 'Real News'
+    elif (predict_NB == 1):
+      detect_NB = 'Real News'
+    
+    #Fungsi Prediksi SVM
+    dense_data_input = loaded_vec.fit_transform([teks]).toarray()
+    predict_SVM = model_SVM.predict(dense_data_input)
+
+    if (predict_SVM == 0):
+      detect_SVM = 'Fake News'
+    if (predict_SVM == 1):
+      detect_SVM = 'Real News'
+
+    # memasukan ke dalam data base
+    sql = "insert into hasil(berita, hasilNB, hasilSVM) values(%s,%s,%s)"
+    val = (teks, detect_NB, detect_SVM)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    
+    #hasil
+    st.success(f'Prediksi Naive Bayes = {detect_NB}')
+    st.success(f'Prediksi Support Vector Machine = {detect_SVM}')
+    st.success('Record Created Successfully!!!')
+
+# menunjukan hasil
+if menu == 'History':
+  st.subheader('Hasil Prediksi')
+  mycursor.execute('select * from hasil')
+  result = mycursor.fetchall()
+  df = pd.DataFrame(result, columns=mycursor.column_names)
+  st.dataframe(df)
   
-  #Fungsi Prediksi SVM
-  dense_data_input = loaded_vec.fit_transform([clean_teks]).toarray()
-  predict_SVM = model_SVM.predict(dense_data_input)
-
-  if (predict_SVM == 0):
-    fakenews_detection_SVM = 'Fake News'
-  if (predict_SVM == 1):
-    fakenews_detection_SVM = 'Real News'
-
-st.success(f'Prediksi Naive Bayes = {fakenews_detection_NB}')
-st.success(f'Prediksi Support Vector Machine = {fakenews_detection_SVM}')
+  # delete
+  id = st.number_input('masukan angka')
+  if st.button('Delete'):
+    sql = 'delete from hasil where id = %s'
+    val = (id,)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    st.success('Record Deleted Successfully!!!')
